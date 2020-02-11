e40a1894fa42546134d39fca568b7ba1d9c29f7d
David Wolff <dcwolff218@gmail.com>
2020-02-11 21:19:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,7 +77,6 @@ end
 
 def checkout(cart, coupons)
   total = 0
- # new_cart = []
   new_cart = consolidate_cart(cart)
   coupon = apply_coupons(new_cart, coupons)
   discount = apply_clearance(coupon)
